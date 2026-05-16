f98ea458710b6a47581bf32e00ce35726294fe54
ishanxmd <ishanmadusanke548@gmail.com>
2026-05-16 23:32:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const {
     downloadMediaMessage, 
     generateWAMessageFromContent, 
     proto 
-} = require('dct-dula-baileys');
+} = require('@whiskeysockets/baileys');
 const express = require('express');
 const app = express();
 __path = process.cwd()
