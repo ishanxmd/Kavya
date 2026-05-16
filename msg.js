9fdcd29f7a460943efbb8f1a9b0f187e104620d6
ishanxmd <ishanmadusanke548@gmail.com>
2026-05-16 23:33:13 +0530

    Fix require statement for baileys module

--- msg.js
+++ msg.js
@@ -2,7 +2,7 @@ const {
     proto,
     downloadContentFromMessage,
     getContentType
-} = require('dct-dula-baileys')
+} = require('@whiskeysockets/baileys')
 const fs = require('fs')
 
 // --- Media Download Function ---
